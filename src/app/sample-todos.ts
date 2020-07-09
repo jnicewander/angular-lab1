356a15e77e3e45d8900fbedc11bb84befb07708f
Justin Nicewander <jnicewander@live.com>
2020-07-08 20:25:27 -0400

    Build specification 1 - completed.

--- src/app/sample-todos.ts
+++ src/app/sample-todos.ts
@@ -1,32 +1,32 @@
-import { Todo } from './todo.interface';
+// import { Todo } from '../todo.interface';
 
-export const TODOLIST: Todo[] = [
-    {
-        task: 'Assimilate with humans of Earth',
-        completed: true
-    },
-    {
-        task: 'Covertly collect specimen samples',
-        completed: false
-    },
-    {
-        task: 'Locate prisoner holding cells inside Area 51',
-        completed: true
-    },
-    {
-        task: 'Refuel UFO',
-        completed: false
-    },
-    {
-        task: 'Abduct cows',
-        completed: true
-    },
-    {
-        task: 'Conquer the world leaders located inside the International House of Pancakes',
-        completed: false
-    },
-    {
-        task: 'Do more alien stuff',
-        completed: false
-    }
-]
+// export const TODOLIST: Todo[] = [
+//     {
+//         task: 'Assimilate with humans of Earth',
+//         completed: true
+//     },
+//     {
+//         task: 'Covertly collect specimen samples',
+//         completed: false
+//     },
+//     {
+//         task: 'Locate prisoner holding cells inside Area 51',
+//         completed: true
+//     },
+//     {
+//         task: 'Refuel UFO',
+//         completed: false
+//     },
+//     {
+//         task: 'Abduct cows',
+//         completed: true
+//     },
+//     {
+//         task: 'Conquer the world leaders located inside the International House of Pancakes',
+//         completed: false
+//     },
+//     {
+//         task: 'Do more alien stuff',
+//         completed: false
+//     }
+// ]
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
-import { TODOLIST } from '../sample-todos';
+import { NgForm } from '@angular/forms';
+import { Todo } from '../todo.interface';
+
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
+
 export class TodoComponent {
-  todolist = TODOLIST;
+
+  todoArray: Todo[] = [];
 
   markComplete(el) {
     el.completed = true;
+  }
+
+  addTask(addTodoInputForm: NgForm) {
+    this.todoArray.push(
+      {
+        task: addTodoInputForm.value.todoInput,
+        completed: false
+      }
+    )
+
+
+    console.log(addTodoInputForm.value);
+    console.log(this.todoArray);
   }
 }
 
